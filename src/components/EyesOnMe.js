d5f6dad7343058827a128a15055094a926b943fa
Brian Smith <brian.bubbam@gmail.com>
2021-06-15 20:49:59 -0500

    commit tree 76d39c423403001c84858e6a3d3a1e1b91c29e58

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function Button(event) {
+        event.preventDefault()
+    }
+    function onFocus(e) {
+        if (e.currentTarget === e.target) {
+            console.log('Good!');
+          }
+    }
+    function onBlur(e) {
+        if (e.currentTarget === e.target) {
+            console.log('Hey! Eyes on me!');
+          }
+    }
+    return (
+        <div>
+            <button onClick={Button} onFocus={onFocus} onBlur={onBlur} >
+                Eyes on me
+            </button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,26 @@
 // Code Keypad Component Here
+
+function Keypad() {
+
+    function Input(event){
+        event.preventDefault()
+        console.log('Entering password...')
+    }
+
+    return(
+        <div>
+            <input
+                type="password"
+                name="password"
+                onChange={Input}
+                placeholder="password"
+            />
+
+            <button name="filter" onChange={Input}>
+                submit
+            </button>
+        </div>
+    )
+}
+
+export default Keypad;
